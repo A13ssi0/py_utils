4d816fc21f25eb9f1c31a860976ca596e9bc73f5
A13ssi0 <alessio.palatella@gmail.com>
2025-04-07 15:39:46 +0200

    Added logbandpower

--- signal_processing.py
+++ signal_processing.py
@@ -1,6 +1,30 @@
 import numpy as np
+import pandas as pd
+from py_utils.eeg_managment import proc_pos2win
+
 from scipy.signal import butter, lfilter
 from tqdm import tqdm
+
+
+def get_trNorm_covariance_matrix(data, events, windowsLength, windowsShift, fc, substractWindowMean=True, dispProgress=True):
+    cov_events = events.copy()
+    if isinstance(events, pd.DataFrame):
+        # [samples] --> [windows]
+        cov_events['pos'] = proc_pos2win(cov_events['pos'], windowsShift*fc, 'backward', windowsLength*fc)
+        cov_events['dur'] = [ int(x) for x in cov_events['dur']/(windowsShift*fc)+1 ]
+
+    n_bandranges, nsamples, nchannels = data.shape
+
+    nwindows = int((nsamples-windowsLength*fc)/(windowsShift*fc))+1
+
+    Cov = np.empty((n_bandranges, nwindows, nchannels, nchannels))
+
+    if dispProgress:
+        print(' - Computing covariance matrices on the band ranges')
+    for bId in range(n_bandranges):
+        ccov = get_sliding_covariance_trace_normalized(data[bId],  windowsLength*fc, windowsShift*fc, substractWindowMean, dispProgress=dispProgress)    # covariances matrix
+        Cov[bId] = ccov
+    return  [Cov, cov_events]
 
 
 def get_bandranges(signal, bandranges, fs, filter_order):
@@ -34,6 +58,14 @@ def get_covariance_matrix_traceNorm(data, substractWindowMean=True):
     t_cov = data.T @ data
     cov =  t_cov  / np.trace(t_cov)
     return cov
+
+
+def logbandpower(data, fs, slidingWindowLength=None):
+    data_sqr = data ** 2
+    if slidingWindowLength is not None:
+        b = np.ones(slidingWindowLength * fs) / (slidingWindowLength * fs)
+        data_sqr = lfilter(b, 1, data_sqr, axis=0)
+    return np.log(data_sqr)
 
 
 # # ---------------------- ONLINE ----------------------

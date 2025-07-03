Corrected concept errors. Code normalization

data_managment.py
@@ -61,15 +61,13 @@ def get_files(path):
             break
         filenames += chosen_files
         
-    if filenames[0,-4:]=='.mat':
-        signal, events_dataFrame, fs = load_mat_files(filenames)
-    elif filenames[0,-4:]=='.gdf':
-        signal, events_dataFrame = load_gdf_files(filenames)
-        fs = None
+    if filenames[0][-4:]=='.mat':
+        signal, events_dataFrame, h, _ = load_mat_files(filenames)
+    elif filenames[0][-4:]=='.gdf':
+        signal, events_dataFrame, h = load_gdf_files(filenames)
     
-    directory_path = dirname(filenames[0])
     root.destroy()
-    return [signal, events_dataFrame, fs, directory_path]
+    return [signal, events_dataFrame, h, list(filenames)]
 
 
 def load_mat_files(filenames):
@@ -109,7 +107,7 @@ def load_mat_files(filenames):
         else:
             d['prt'].append([-1]*len(h['EVENT']['DUR']))
         last_day = file.split('/')[-2]
-        signal.append(data['s'][:,:-1])
+        signal.append(data['s'])
         eeg_dim_tot += data['s'].shape[0]
 
 
@@ -121,11 +119,9 @@ def load_mat_files(filenames):
     d['day'] = [ int(x) for x in np.concatenate(d['day']) ]
     d['prt'] = [ int(x) for x in np.concatenate(d['prt']) ]
     #d['ses_vector'] = [ int(x) for x in np.concatenate(d['ses_vector']) ]
-
-    fs = int(h['SampleRate'])
     
     events_dataFrame = pd.DataFrame(data=d)
-    return signal, events_dataFrame, fs, dates
+    return signal, events_dataFrame, h, dates
 
 
 def load_gdf_files(filenames):
@@ -164,13 +160,14 @@ def load_gdf_files(filenames):
     #d['ses_vector'] = [ int(x) for x in np.concatenate(d['ses_vector']) ]
     
     events_dataFrame = pd.DataFrame(data=d)
-    return signal, events_dataFrame
+    return signal, events_dataFrame, h
 
 
 def load(filename):
     return joblib.load(filename)
 
 def save(filename, variable):
+    if not filename.endswith('.joblib'):    filename += '.joblib'
     joblib.dump(variable, filename)
 
 

eeg_managment.py
@@ -12,7 +12,7 @@ def select_channels(signal, wantedChannels, actualChannels=[]):
             'C4', 'CP5', 'CP1', 'CP2', 'CP6',  'P3', 'Pz',  'P4',  'F1',  'F2', 'FC3', 
             'FCZ', 'FC4', 'C5',  'C1',  'C2',  'C6', 'CP3', 'CP4',  'P5',  'P1', 'P2',  'P6'])
         
-    elif signal.shape[1] == 16:
+    elif signal.shape[1] == 16 and len(actualChannels)==0:
         actualChannels =  np.array(['Fz', 'FC3', 'FC1', 'FCz', 'FC2', 'FC4', 'C3','C1', 'Cz', 'C2', 'C4', 'CP3', 'CP1', 'CPz','CP2', 'CP4'])
     
     boolean_list = get_channels_bool(wantedChannels, actualChannels)

signal_processing.py
@@ -5,31 +5,35 @@ from scipy.signal import butter, lfilter, lfilter_zi
 from tqdm import tqdm
 
 
-def get_trNorm_covariance_matrix(data, events, windowsLength, windowsShift, fc, substractWindowMean=True, dispProgress=True):
+def get_trNorm_covariance_matrix(data, events, windowsLength, windowsShift, fs, substractWindowMean=True, dispProgress=True):
     cov_events = events.copy()
     if isinstance(events, pd.DataFrame):
         # [samples] --> [windows]
-        cov_events['pos'] = proc_pos2win(cov_events['pos'], windowsShift*fc, 'backward', windowsLength*fc)
-        cov_events['dur'] = [ int(x) for x in cov_events['dur']/(windowsShift*fc)+1 ]
+        cov_events['pos'] = proc_pos2win(cov_events['pos'], windowsShift*fs, 'backward', windowsLength*fs)
+        cov_events['dur'] = [ int(x) for x in cov_events['dur']/(windowsShift*fs)+1 ]
 
     n_bandranges, nsamples, nchannels = data.shape
 
-    nwindows = int((nsamples-windowsLength*fc)/(windowsShift*fc))+1
+    nwindows = int((nsamples-windowsLength*fs)/(windowsShift*fs))+1
 
     Cov = np.empty((n_bandranges, nwindows, nchannels, nchannels))
 
     if dispProgress:
         print(' - Computing covariance matrices on the band ranges')
     for bId in range(n_bandranges):
-        ccov = get_sliding_covariance_trace_normalized(data[bId],  windowsLength*fc, windowsShift*fc, substractWindowMean, dispProgress=dispProgress)    # covariances matrix
+        ccov = get_sliding_covariance_trace_normalized(data[bId],  windowsLength*fs, windowsShift*fs, substractWindowMean, dispProgress=dispProgress)    # covariances matrix
         Cov[bId] = ccov
     return  [Cov, cov_events]
 
 
-def get_bandranges(signal, bandranges, fs, filter_order):
-    filt_signal = np.empty(tuple([len(bandranges)]) + signal.shape)
+def get_bandranges(signal, bandranges, fs, filter_order, filtType):
+    if len(bandranges) == 0:    return signal
+
+    if len(signal.shape) == 2:  filt_signal = np.empty(tuple([len(bandranges)]) + signal.shape)
+    elif len(signal.shape) == 3:  filt_signal = np.empty(signal.shape)
+
     for i,band in enumerate(bandranges):
-        [b,a] = butter(filter_order,np.array(band)/(fs/2),'bandpass')
+        [b,a] = butter(filter_order,np.array(band)/(fs/2), filtType)
         filt_signal[i, :, :] = lfilter(b,a,signal,axis=0)
     return filt_signal
 